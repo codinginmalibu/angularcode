80c739f39bd25b4d0159c9df031e0aa750135d33
Shailen Mistry <smistry@dandb.com>
2015-08-31 08:04:07 -0700

    multiple gems

--- app.js
+++ app.js
@@ -2,16 +2,23 @@
 	var app = angular.module('gemStore', []);
 
 	app.controller('StoreController', function(){
-		this.product = gem;
+		this.products = gems;
 	});
 
-	var gem = {
-		name: 'Dodecahedron',
-		price: 3.00,
-		description: 'a nice gem',
-		canPurchase: true,
-		soldOut: true,
-	}
+	var gems = [
+		{
+			name: 'Dodecahedron',
+			price: 3.00,
+			description: 'a nice gem',
+			canPurchase: true,
+		},
+		{
+			name: 'Pentagonal Gem',
+			price: 12.00,
+			description: 'Five Sided Gem',
+			canPurchase: false,
+		},
+	];
 })();
 
 
