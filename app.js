2a958702236c0499d1f9150c8f064572401a03ce
Shailen Mistry <smistry@dandb.com>
2015-09-02 06:36:05 -0700

    added controller and tabs

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 (function(){
 	var app = angular.module('gemStore', []);
 
+	app.controller('PanelController', function(){
+		this.tab = 1;
+		this.selectTab = function(setTab){
+			this.tab = setTab;
+		};
+		this.isSelected = function(checkTab){
+			return this.tab === checkTab;
+		}
+	});
 	app.controller('StoreController', function(){
 		this.products = gems;
 	});
