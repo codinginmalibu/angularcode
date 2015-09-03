a66d0ddc5bfe0fe4b064ebe140acd257b384d5aa
Shailen Mistry <smistry@dandb.com>
2015-09-03 06:45:28 -0700

    added form

--- app.js
+++ app.js
@@ -8,8 +8,17 @@
 		};
 		this.isSelected = function(checkTab){
 			return this.tab === checkTab;
-		}
+		};
 	});
+
+	app.controller('ReviewController', function(){
+		this.review = {};
+		this.addReview = function(product) {
+			product.reviews.push(this.review);
+			this.review = {};
+		};
+	});
+
 	app.controller('StoreController', function(){
 		this.products = gems;
 	});
@@ -20,13 +29,37 @@
 			price: 3.00,
 			description: 'a nice gem',
 			canPurchase: true,
+			reviews: [
+				{
+					stars: 5,
+					body: "I love this gem",
+					author: "joe@thomas.com",
+				},
+				{
+					stars: 4,
+					body: "Good value for the price",
+					author: "flat@landers.com",
+				},
+			]
 		},
 		{
 			name: 'Pentagonal Gem',
 			price: 12.00,
 			description: 'Five Sided Gem',
 			canPurchase: false,
-		},
+			reviews: [
+				{
+					stars: 3,
+					body: "Good quality",
+					author: "mike@smith.com",
+				},
+				{
+					stars: 4,
+					body: "Nice Gem",
+					author: "bo@duke.com",
+				},
+			]
+		},	
 	];
 })();
 
