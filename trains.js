5cb760c6580ef43a82b862317d03fa15b62f73ca
Shailen Mistry <smistry@dandb.com>
2015-09-27 12:57:23 -0700

    add js functions

--- trains.js
+++ trains.js
@@ -27,8 +27,37 @@ for (trainNumber = 1; trainNumber <= totalTrains; trainNumber++) {
 function multiplyTrio (a, b, c) {
   return a*b*c;
 }
+var x1 = multiplyTrio(1, 2, 3);
+console.log("multiplyTrio = " + x1);
 
 function mystery(x, y) {
   return (4 * x * y) + (3 * y + 5);
 }
+var x2 = mystery(10, 7);
+console.log("mystery = " + x2);
+
+function changePowerTotal (totalPowerGenerated, generatorID, generatorStatus, amountOfPowerProduced) {
+  if (generatorStatus == "on") {
+    totalPowerGenerated += amountOfPowerProduced;
+    alert("Generator #" + generatorID + " is now on, adding " + amountOfPowerProduced + " MW, for a total of " + totalPowerGenerated + " MW!")
+  }
+  else {
+    totalPowerGenerated -= amountOfPowerProduced;
+    alert("Generator #" + generatorID + " is now off, removing " + amountOfPowerProduced + " MW, for a total of " + totalPowerGenerated + " MW!")
+  }
+  return totalPowerGenerated;
+}
+
+var list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+list.push(10);
+list.push(11);
+list.pop();
+console.log("list = " + list);
+
+// Test out unnamed functions
+var diff = function(a,b) {
+  return a*a + b*b;
+}
+console.log("unnamed function diff is = " + diff);
+
 
