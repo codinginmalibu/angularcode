24d689893acd5e750e849432bed5da25a82933b5
Shailen Mistry <smistry@dandb.com>
2015-10-03 09:54:42 -0700

    complete objects, add enumeration

--- objects.js
+++ objects.js
@@ -109,3 +109,40 @@ lighthouseRock.addBulb("Sight Slayer", 1800);
 lighthouseRock.addBulb("Burner of Souls", 7500);
 
 
+var vehicle3 = {
+  type: "Submarine", capacity: 8, storedAt: "Underwater Outpost",
+  ranger1: {name: "Gregg Pollack", skillz: "Lasering", dayOff: "Friday"},
+  ranger2: {name: "Bijan Boustani", skillz: "Working", dayOff: "Saturday"},
+  ranger3: {name: "Ashley Smith", skillz: "Torpedoing", dayOff: "Friday"},
+  ranger4: {name: "Mark Krupinski", skillz: "Sniping", dayOff: "Wednesday"},
+  numRangers: 4
+};
+
+function relieveDuty(vehicle, day) {
+	var offDuty = [];
+	var onDuty = [];
+	for (var i = 1; i <= vehicle.numRangers; i++) {
+		if (vehicle["ranger" + i]["dayOff"] == day) {
+			offDuty.push(vehicle["ranger" + i]);
+		}
+		else {
+			onDuty.push(vehicle["ranger" + i]);
+		}
+		delete vehicle["ranger" + i];
+	}
+
+	vehicle.numRangers -= offDuty.length;
+
+	for (var i = 1; i <= vehicle.numRangers; i++) {
+		vehicle["ranger" + i] = onDuty[i-1];
+	}
+
+	return offDuty;
+}
+
+var offToday = relieveDuty(vehicle3, "Friday")
+
+console.log("vehicle3.numRangers = " + vehicle3.numRangers);
+console.log("offToday.length = " + offToday.length);
+console.dir(offToday);
+
