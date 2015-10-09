ae31b10dad8fc50e186ea5545c6f894af0f530bb
Shailen Mistry <smistry@dandb.com>
2015-10-09 07:19:43 -0700

    add to object and array prototype

--- prototypes.js
+++ prototypes.js
@@ -7,6 +7,22 @@ var canyonCows = [
   {name: "Samson", type: "bull", hadCalf: null},
   {name: "Delilah", type: "cow", hadCalf: null}
 ];
+var valleyCows = [
+  {name: "Danielle", type: "cow", hadCalf: null},
+  {name: "Brittany", type: "cow", hadCalf: "Christina"},
+  {name: "Jordan", type: "bull", hadCalf: null},
+  {name: "Trevor", type: "bull", hadCalf: null},
+  {name: "Christina", type: "calf", hadCalf: null},
+  {name: "Lucas", type: "bull", hadCalf: null}
+];
+var forestCows = [
+  {name: "Legolas", type: "calf", hadCalf: null},
+  {name: "Gimli", type: "bull", hadCalf: null},
+  {name: "Arwen", type: "cow", hadCalf: null},
+  {name: "Galadriel", type: "cow", hadCalf: null},
+  {name: "Eowyn", type: "cow", hadCalf: "Legolas"}
+];
+
 
 Array.prototype.countCattle = function(kind) {
 	var numKind = 0;
@@ -16,7 +32,29 @@ Array.prototype.countCattle = function(kind) {
 		}
 	}
 	return numKind;
-}
+};
 
 console.log("Num Cows = " + canyonCows.countCattle("cow"));
 console.log("Num Bulls = " + canyonCows.countCattle("bull"));
+console.log("Total = " + ( canyonCows.countCattle("calf") + valleyCows.countCattle("bull") + forestCows.countCattle("cow") ) );
+// alert(  ( canyonCows.countCattle("calf") + valleyCows.countCattle("bull") + forestCows.countCattle("cow") )  );
+
+Object.prototype.noCalvesYet = function() {
+	if (this.type == "cow" && this.hadCalf == null) {
+		return true;
+	}
+	else {
+		return false;
+	}
+};
+Array.prototype.countForBreeding = function() {
+	var numToBreed = 0;
+	for (var i = 0; i < this.length; i++) {
+		if ( this[i].noCalvesYet() ) {
+			numToBreed++;
+		}
+	}
+	return numToBreed;
+};
+
+console.log("Num to Breed = " + valleyCows.countForBreeding() );
