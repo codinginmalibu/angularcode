c7bcc0beaea8c3b2de100334344ab292ac11f25e
Shailen Mistry <smistry@dandb.com>
2015-10-14 23:05:35 -0700

    override valueOf

--- posts.js
+++ posts.js
@@ -72,13 +72,45 @@ for (var i = 0; i < genericPost.postArray.length; i++) {
   }
 }
 
-function Fencepost (x, y, postNum) {
+function Fencepost(x, y, postNum) {
   this.x = x;
   this.y = y;
   this.postNum = postNum;
-  this.connectionsTo: undefined;
-  this.sendRopeTo = function(connectedPost) {
-    this.connectionsTo.push(connectedPost);
-  }
-
+  this.connectionsTo = [];
 }
+
+Fencepost.prototype = {
+  sendRopeTo: function(connectedPost) {
+    this.connectionsTo.push(connectedPost);
+  };
+  removeRope: function(removeTo) {
+    var temp = [];
+    for (var i = 0; i < this.connectionsTo.length; i++) {
+      if (this.connectionsTo[i].postNum != removeTo) {
+        temp.push(this.connectionsTo[i]);
+      }
+    }
+    this.connectionsTo = temp;
+  };
+  movePost: function(x, y) {
+    this.x = x;
+    this.y = y;
+  };
+};
+
+
+// create post18, post19, and post20
+var post18 = new Fencepost(-3, 4, 18);
+var post19 = new Fencepost(5, -1, 19);
+var post20 = new Fencepost(-2, 10, 20);
+
+
+// establish post connections
+post18.sendRopeTo(post20);
+post20.sendRopeTo(post18);
+post18.sendRopeTo(post19);
+post19.sendRopeTo(post18);
+
+Fencepost.prototype.valueOf = function() {
+  return Math.sqrt(this.x*this.x + this.y*this.y);
+};
